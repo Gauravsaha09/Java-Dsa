609a231f0e7a35b72523028577bacfa0481a3393
Gaurav Kumar <neonxgaurav9@gmail.com>
2025-09-29 23:58:42 +0530

    Added new Java programs

--- 10-Arrays/Q1.java
+++ 10-Arrays/Q1.java
@@ -6,8 +6,13 @@ public class Q1 {
         marks [1]= 80;
         marks [2] = 99;
     
-       System.out.println(marks[0]);
-       System.out.println(marks[1]);
-       System.out.println(marks[2]);
+    //    System.out.println(marks[0]);
+    //    System.out.println(marks[1]);
+    //    System.out.println(marks[2]);
+
+      for (int i = 0; i < 3; i++) {
+          System.out.println(marks[i]);
+      }
+
     }
 }
